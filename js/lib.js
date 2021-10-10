4f512d078d688c799a1efc8ecea6bea5b0adb8d1
boonjiashen <boonjiashen@users.noreply.github.com>
2021-10-10 07:47:42 +0000

    deploy: 542bff4bc329d31191ae7943e0b7d5cae27a0a7a

--- js/lib.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var Foo = /** @class */ (function () {
-    function Foo(a) {
-        this.a = a;
-    }
-    return Foo;
-}());
-var foo = new Foo("I'm in foo");
-console.log(foo.a);
