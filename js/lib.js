20c600fa6c95c257be6b79ff1dbe1151a2e1ce8f
boonjiashen <boonjiashen@users.noreply.github.com>
2021-10-13 22:29:15 +0000

    deploy: 5d486af8b4dfe312a89e459a315b59c47d36e755

--- js/lib.js
+++ js/lib.js
@@ -16,6 +16,8 @@ function getRectOfCanvas(canvas) {
 }
 var $c = (0, jquery_1.default)("canvas");
 var canvas = $c[0];
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 var ctx = canvas.getContext("2d");
 function clearCanvas(context) {
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
@@ -23,19 +25,35 @@ function clearCanvas(context) {
 var img = new Image();
 img.src = "images/chicken.png";
 var w = ctx.canvas.width / 5;
-var initRect = new Rect_1.Rect(0, 0, w, w);
+var initRect = new Rect_1.Rect(ctx.canvas.width / 2, ctx.canvas.height / 2, w, w);
 var init_vel = new Velocity_1.Velocity(8, 0);
 var currRect = initRect;
 var curr_vel = init_vel;
 var isAnimationEnabled = true;
+function chooseAtRandom(l) {
+    var index = Math.floor(Math.random() * l.length);
+    return l[index];
+}
+function generateVelocity() {
+    var speed = 8;
+    return chooseAtRandom([
+        new Velocity_1.Velocity(0, speed),
+        new Velocity_1.Velocity(speed, 0),
+        new Velocity_1.Velocity(0, -speed),
+        new Velocity_1.Velocity(-speed, 0),
+    ]);
+}
+setInterval(function () {
+    curr_vel = generateVelocity();
+}, 300);
 function animate() {
     clearCanvas(ctx);
-    ctx.drawImage(img, currRect.x, currRect.y, currRect.width, currRect.height);
     currRect = currRect.refresh(curr_vel);
-    var isInCanvas = currRect.isInside(getRectOfCanvas(ctx.canvas));
-    if (!isInCanvas) {
-        curr_vel.v_x *= -1;
-    }
+    ctx.drawImage(img, currRect.x, currRect.y, currRect.width, currRect.height);
+    // const isInCanvas = currRect.isInside(getRectOfCanvas(ctx.canvas))
+    // if (!isInCanvas) {
+    //   curr_vel.v_x *= -1
+    // }
     if (isAnimationEnabled) {
         requestAnimationFrame(animate);
     }
