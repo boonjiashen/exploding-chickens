d4de263c00d03e0c19cfd2d68b68762e727cd89c
Jia-Shen Boon <jiashen@gmail.com>
2021-10-16 08:47:58 +0900

    Fix issue with webpack not compiling

--- ts/lib.ts
+++ ts/lib.ts
@@ -1,3 +1,4 @@
+import { assert } from "console";
 import $ from "jquery";
 import { Point } from "./Point";
 import { Rect } from "./Rect";
@@ -13,6 +14,29 @@ function getRectOfCanvas(canvas: any) {
   return new Rect(0, 0, canvas.width, canvas.height)
 }
 
+/**
+ * Gets the coordinates of the i-th frame in a sprint sheet
+ * @param sheetDim Dimensions of the sprite sheet, ignores x/y values
+ * @param frameDim Dimensions of the frame within the sprite sheet, ignores x/y values
+ * @param idx index of the frame to be retrieved
+ */
+function getSpriteFrame(sheetDim: Rect, frameDim: Rect, idx: number) {
+  assert(sheetDim.width % frameDim.width == 0)
+  assert(sheetDim.height % frameDim.height == 0)
+
+  const num_frame_x = sheetDim.width / frameDim.width
+  const num_frame_y = sheetDim.height / frameDim.height
+
+  assert(idx < num_frame_x * num_frame_y)
+
+  return new Rect(
+    (idx % num_frame_x) * frameDim.width,
+    Math.floor(idx / num_frame_x) * frameDim.height,
+    frameDim.width,
+    frameDim.height
+  )
+}
+
 const $c = $("canvas")
 const canvas = $c[0] as HTMLCanvasElement;
 canvas.width = window.innerWidth;
@@ -23,8 +47,8 @@ function clearCanvas(context: any) {
   context.clearRect(0, 0, context.canvas.width, context.canvas.height)
 }
 
-const img = new Image()
-img.src = "images/chicken.png"
+const chicken = new Image()
+chicken.src = "images/chicken.png"
 const w = ctx.canvas.width / 5
 const initRect = new Rect(ctx.canvas.width / 2, ctx.canvas.height / 2, w, w)
 const init_vel = new Velocity(8, 0)
@@ -54,7 +78,7 @@ setInterval(() => {
 function animate() {
   clearCanvas(ctx)
   currRect = currRect.refresh(curr_vel)
-  ctx.drawImage(img, currRect.x, currRect.y, currRect.width, currRect.height)
+  ctx.drawImage(chicken, currRect.x, currRect.y, currRect.width, currRect.height)
   // const isInCanvas = currRect.isInside(getRectOfCanvas(ctx.canvas))
   // if (!isInCanvas) {
   //   curr_vel.v_x *= -1
@@ -67,7 +91,7 @@ function animate() {
   }
 }
 
-img.onload = animate
+chicken.onload = animate
 
 $c.on('click', function($event: any) {
   const canvasRect = this.getBoundingClientRect()
